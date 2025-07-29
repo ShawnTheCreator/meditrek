efe699ba1da5b5fb70cedd39858be7aa507eda87
Shawn <shawnchareka7@gmail.com>
2025-07-29 21:22:29 +0200

    updated routes

--- meditrekfrontend/src/App.tsx
+++ meditrekfrontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContexts';
 import HomePage from './pages/HomePage';
+import SignupPage from './pages/SignUp';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <div className="min-h-screen bg-white">
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/signup" element={<SignupPage />} />
           </Routes>
         </div>
       </Router>
